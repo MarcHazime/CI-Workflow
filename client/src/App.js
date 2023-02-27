6674f8140db085db0f1ab170f55576a0aac3bfa1
Marc Hazime <mhazime@dydu.ai>
2023-02-27 16:25:56 +0100

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.learn react
+          Edit <code>src/App.js</code> and save to reload the app.learn react
         </p>
       </header>
     </div>
